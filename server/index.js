9385b3cb518a29336ccc2246f84e563dac147251
kimi.casamina <kimi.casamina@gmail.com>
2024-11-05 18:36:02 +0800

    build for deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
+import path from "path";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/auth.js";
 import roomsRoute from "./routes/rooms.js";
@@ -17,13 +18,34 @@ dotenv.config();
 connectDB();
 
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/rooms", roomsRoute);
 app.use("/api/hotels", hotelsRoute);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, "client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready..."));
+}
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
