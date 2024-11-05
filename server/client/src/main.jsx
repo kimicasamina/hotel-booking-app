a45d4f312ee958e6ab8d979b9aec41325c495ad0
kimi.casamina <kimi.casamina@gmail.com>
2024-11-05 18:42:48 +0800

    added aiox base url

--- server/client/src/main.jsx
+++ server/client/src/main.jsx
@@ -6,6 +6,9 @@ import App from "./App.jsx";
 import { SearchContextProvider } from "./context/searchContext.jsx";
 import { AuthContextProvider } from "./context/authContext.jsx";
 
+import axios from "axios";
+axios.defaults.baseURL = "https://hotel-booking-app-mernstack.onrender.com";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContextProvider>
--- server/client/vite.config.js
+++ server/client/vite.config.js
@@ -3,25 +3,25 @@ import react from "@vitejs/plugin-react";
 import { config } from "dotenv";
 import path from "path";
 
-config();
+// config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          process.env.VITE_MODE === "production"
-            ? process.env.VITE_CLIENT_URL
-            : "http://localhost:8080",
-      },
+  // define: {
+  //   "process.env": process.env,
+  // },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target:
+  //         process.env.VITE_MODE === "production"
+  //           ? process.env.VITE_CLIENT_URL
+  //           : "http://localhost:8080",
+  //     },
 
-      "/admin": {
-        target: "http://localhost:8081/",
-      },
-    },
-  },
+  //     "/admin": {
+  //       target: "http://localhost:8081/",
+  //     },
+  //   },
+  // },
 });
